df39069fcfc371365156ef34bf922309acf7b125
Reijhanniel Jearl Campos <devcsrj@apache.org>
2017-12-29 23:51:11 +0800

    Parse directly from response input stream
    This commit reduces the memory footprint by removing the intermediate string created when parsing, as described by the previous implementation.

--- retrofit-converter-jspoon/src/main/java/pl/droidsonroids/retrofit2/JspoonResponseBodyConverter.java
+++ retrofit-converter-jspoon/src/main/java/pl/droidsonroids/retrofit2/JspoonResponseBodyConverter.java
@@ -1,19 +1,38 @@
 package pl.droidsonroids.retrofit2;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.ResponseBody;
 import pl.droidsonroids.jspoon.HtmlAdapter;
 import retrofit2.Converter;
 
 class JspoonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
-    private HtmlAdapter<T> htmlAdapter;
 
-    JspoonResponseBodyConverter(HtmlAdapter<T> htmlAdapter) {
+    private final HttpUrl httpUrl;
+    private final HtmlAdapter<T> htmlAdapter;
+
+    JspoonResponseBodyConverter(HttpUrl httpUrl, HtmlAdapter<T> htmlAdapter) {
+        this.httpUrl = httpUrl;
         this.htmlAdapter = htmlAdapter;
     }
 
     @Override
     public T convert(ResponseBody responseBody) throws IOException {
-        return htmlAdapter.fromHtml(responseBody.string());
+        Charset charset = null;
+        MediaType mediaType = responseBody.contentType();
+        if (mediaType != null)
+            charset = mediaType.charset();
+
+        InputStream is = responseBody.byteStream();
+        try {
+            return htmlAdapter.fromInputStream(is, charset, httpUrl.url());
+        } finally {
+            is.close();
+        }
     }
 }
