5980cdf2abadab112c5feaebc9509bc92e16bdfd
cr3ativ3 <cr3ativ3@users.noreply.github.com>
2018-01-24 23:43:09 +0200

    Prevention of ClassLoader or concurrency issues

--- jspoon/src/main/java/pl/droidsonroids/jspoon/Jspoon.java
+++ jspoon/src/main/java/pl/droidsonroids/jspoon/Jspoon.java
@@ -1,14 +1,14 @@
 package pl.droidsonroids.jspoon;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Coordinates binding between HTML values and Java objects.
  */
 public class Jspoon {
 
-    private Map<String, HtmlAdapter<?>> adapterCache;
+    private Map<Class<?>, HtmlAdapter<?>> adapterCache;
 
     /**
      * Creates a new Jspoon instance.
@@ -20,7 +20,7 @@ public class Jspoon {
     }
 
     private Jspoon() {
-        this.adapterCache = new LinkedHashMap<>();
+        this.adapterCache = new ConcurrentHashMap<>();
     }
 
     /**
@@ -30,14 +30,16 @@ public class Jspoon {
      * @param <T> Class for creating objects
      * @return {@link HtmlAdapter} instance
      */
+
+    @SuppressWarnings("unchecked")
     public <T> HtmlAdapter<T> adapter(Class<T> clazz) {
-        String key = clazz.getName();
-        if (adapterCache.containsKey(key)) {
-            return (HtmlAdapter<T>) adapterCache.get(key);
+        HtmlAdapter<T> htmlAdapter;
+        if (adapterCache.containsKey(clazz)) {
+            htmlAdapter = (HtmlAdapter<T>) adapterCache.get(clazz);
         } else {
-            HtmlAdapter<T> htmlAdapter = new HtmlAdapter<>(this, clazz);
-            adapterCache.put(key, htmlAdapter);
-            return htmlAdapter;
+            htmlAdapter = new HtmlAdapter<>(this, clazz);
+            adapterCache.put(clazz, htmlAdapter);
         }
+        return htmlAdapter;
     }
 }
