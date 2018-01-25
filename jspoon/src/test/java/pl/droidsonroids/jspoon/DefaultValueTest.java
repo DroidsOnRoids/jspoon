fab862bf7d1844a15329411a0d9821ec9daa9eb7
cr3ativ3 <cr3ativ3@users.noreply.github.com>
2018-01-25 20:50:26 +0200

    Possibility to default to value initialy set on field
    defValue should be a fallback for css selector parse result, a default
    value for any type of field can also be set by initializg that field to
    whatever.
    Also fixed default attribute parsing.

--- jspoon/src/test/java/pl/droidsonroids/jspoon/DefaultValueTest.java
+++ jspoon/src/test/java/pl/droidsonroids/jspoon/DefaultValueTest.java
@@ -1,10 +1,12 @@
 package pl.droidsonroids.jspoon;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import org.junit.Before;
 import org.junit.Test;
-import pl.droidsonroids.jspoon.annotation.Selector;
 
-import static org.junit.Assert.assertEquals;
+import pl.droidsonroids.jspoon.annotation.Selector;
 
 public class DefaultValueTest {
     private Jspoon jspoon;
@@ -15,15 +17,23 @@ public class DefaultValueTest {
     }
 
     private static class Model {
-        @Selector(value = "span", defValue = "NO_VALUE") String text;
+        @Selector(value = "span", defValue = "DEFAULT_VALUE") String text;
         @Selector(value = "p", defValue = "-100") int number;
+
+        @Selector(value = "span", defValue = "hello") String text2 = "world";
+        @Selector(value = "span") String text3 = "helloworld";
+        @Selector(value = "span") String text4;
     }
 
     @Test
     public void defaultValueTest() {
         HtmlAdapter<Model> htmlAdapter = jspoon.adapter(Model.class);
         Model model = htmlAdapter.fromHtml("<div></div>");
-        assertEquals(model.text, "NO_VALUE");
-        assertEquals(model.number, -100);
+        //assertEquals("NO_VALUE", model.text);  // bad test case when set defValue is the same result as unset defValue
+        assertEquals("DEFAULT_VALUE", model.text); // since defValue explicitly defined
+        assertEquals("hello", model.text2); // defValue takes precedent as its whatever would be parsed from Element
+        assertEquals("helloworld", model.text3); // no defValue, let's leave whatever is set
+        assertNull(model.text4); // should not be set to anything silently if developer did not set a defValue
+        assertEquals(-100, model.number);
     }
 }
